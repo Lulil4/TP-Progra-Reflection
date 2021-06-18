79652163f3e7e7a076beb829a91c74ff1e9203a7
lu <asd>
2021-06-18 01:20:36 -0300

    terminado? creo

--- FrameworkTPProgra/src/Servicios/Consultas.java
+++ FrameworkTPProgra/src/Servicios/Consultas.java
@@ -13,6 +13,7 @@ import Anotaciones.Id;
 import Anotaciones.Tabla;
 import Utilidades.UBean;
 import Utilidades.UConexion;
+import testborrar.Persona;
 
 public class Consultas {
 
@@ -104,5 +105,117 @@ public class Consultas {
 		}
 
 		return lista;
+	}
+	
+	public static void modificar(Object o) {
+		//Modifica todo excepto ID (se usara en el where)
+		try {
+			ArrayList<Field> fields = UBean.obtenerAtributos(o);
+			
+			UConexion con = UConexion.generarObjeto();
+			con.abrirConexion();
+			String query = "UPDATE " + o.getClass().getAnnotation(Tabla.class).nombre() + " SET ";
+			String id = null;
+			for (Field f : fields) {
+				if (f.getAnnotation(Id.class) != null) {
+					 id = " WHERE " + f.getName().toString() + " = " + UBean.ejecutarGet(o, f.getName().toString());
+				}
+				else {
+					if(f.getType() == String.class) {
+						query += f.getName().toString() + " = '" + UBean.ejecutarGet(o, f.getName().toString()) + "', ";
+					}
+					else {
+						query += f.getName().toString() + " = " + UBean.ejecutarGet(o, f.getName().toString()) + ", ";
+					}
+				}
+			}
+			query = query.substring(0, query.length()-2);
+			query += " ";
+			query += id;
+			PreparedStatement pst = (PreparedStatement) UConexion.c
+					.prepareStatement(query);
+	
+			if(pst != null) {
+				pst.execute();
+			}
+
+			UConexion.cerrarConexion();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void eliminar(Object o) {
+		try {
+			ArrayList<Field> fields = UBean.obtenerAtributos(o);
+			
+			UConexion con = UConexion.generarObjeto();
+			con.abrirConexion();
+			PreparedStatement pst = null;
+			for (Field f : fields) {
+				if (f.getAnnotation(Id.class) != null) {
+					pst = (PreparedStatement) UConexion.c
+							.prepareStatement("DELETE FROM " + o.getClass().getAnnotation(Tabla.class).nombre() + " WHERE " + f.getName().toString() + " = " + UBean.ejecutarGet(o, f.getName().toString()));
+					break;
+				}
+			}
+			
+			if(pst != null) {
+				pst.execute();
+			}
+
+			UConexion.cerrarConexion();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object obtenerPorId(Class<?> c, Object o) {
+		Object obtenido = null;
+
+		try {
+			Constructor[] constructores = c.getConstructors();
+			for (Constructor con1 : constructores) {
+				if (con1.getParameterCount() == 0) {
+					obtenido = con1.newInstance();
+					break;
+				}
+			}
+			ArrayList<Field> fields = UBean.obtenerAtributos(obtenido);
+			UConexion con = UConexion.generarObjeto();
+			con.abrirConexion();
+			
+			PreparedStatement pst = null;
+			for (Field f : fields) {
+				if (f.getAnnotation(Id.class) != null) {
+					pst = (PreparedStatement) UConexion.c
+							.prepareStatement("SELECT * FROM " + c.getAnnotation(Tabla.class).nombre() + " WHERE " + f.getName().toString() + " = " + o.toString());
+				break;
+				}
+			}
+			
+			
+			pst = (PreparedStatement) UConexion.c
+					.prepareStatement("SELECT * FROM " + c.getAnnotation(Tabla.class).nombre() + " WHERE id = " + o.toString());
+			ResultSet rs = pst.executeQuery();
+			int i = 1;
+			while (rs.next()) {
+				for (Field f : fields) {
+					if (f.getAnnotation(Id.class) == null) {
+						UBean.ejecutarSet(obtenido, f.getName().toString(), rs.getString(i));
+					} else {
+						UBean.ejecutarSet(obtenido, f.getName().toString(), rs.getInt(i));
+					}
+
+					i++;
+				}
+				break;
+			}	
+			UConexion.cerrarConexion();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return obtenido;
 	}
 }
--- FrameworkTPProgra/src/testborrar/Program.java
+++ FrameworkTPProgra/src/testborrar/Program.java
@@ -13,35 +13,54 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		//Persona p = new Persona();
-
-		// Test 1a obtenerAtributos
-		//ArrayList<Field> fields = UBean.obtenerAtributos(p);
-
-		//for (Field f : fields) {
-		//	System.out.println(f.getName());
-		//}
-
-		// Test 1b ejecutarSet
-		//UBean.ejecutarSet(p, "id", 8);
-		//UBean.ejecutarSet(p, "nombre", "Lucia");
-		//UBean.ejecutarSet(p, "apellido", "Morel");
-
-		// Test 1c ejecutarGet
-		//System.out.println(UBean.ejecutarGet(p, "nombre"));
-		//System.out.println(UBean.ejecutarGet(p, "apellido"));
-
-		// Test 3a guardar (por ahora solo trae la query armada)
-
-		// System.out.println(Consultas.guardar(p));
-
-		
+		Persona p = new Persona();
+		/*
+		Test 1a obtenerAtributos
+		ArrayList<Field> fields = UBean.obtenerAtributos(p);
+		for (Field f : fields) {
+			System.out.println(f.getName());
+		}
+		*/
+		/*
+		Test 1b ejecutarSet
+		UBean.ejecutarSet(p, "id", 8);
+		UBean.ejecutarSet(p, "nombre", "Lucia");
+		UBean.ejecutarSet(p, "apellido", "Morel");
+		*/
+		/*
+		Test 1c ejecutarGet
+		System.out.println(UBean.ejecutarGet(p, "nombre"));
+		System.out.println(UBean.ejecutarGet(p, "apellido"));
+		*/
+		/*
+		Test 3a guardar (por ahora solo trae la query armada)
+		System.out.println(Consultas.guardar(p));
+		*/
+		/*
+		Test 3b (modificar)
+		Persona p = new Persona("Demian", "Murray");
+		p.setId(1);
+		Consultas.modificar(p);
+		*/
+		/*
+		Test 3c (eliminar)
+		Persona p = new Persona();
+		p.setId(2);
+		Consultas.eliminar(p);
+		*/
+		/*
+		Test 3d (traer por id)
+		Persona p = (Persona)Consultas.obtenerPorId(Persona.class, 1);
+		System.out.println(p.toString());
+		*/
+		/*
+		Test 7 (traer todos)
 		ArrayList<Persona> arr = (ArrayList<Persona>) Consultas.obtenerTodos(Persona.class);
 			
 		for (Object o : arr) {
 			System.out.println(o.toString());
 		}
-		 
+		*/
 	}
 
 }
